c9d20f92a75b4dca71b7cd47236460a2bbdabbee
adarshuday <adarshuday@adarsh-mbp>
2021-02-13 20:48:13 -0500

    Print out even number from 0 to 100

---
+++ Assignment_1/src/assignment_1/Assignment_1.java
@@ -0,0 +1,18 @@
+package assignment_1;
+
+
+public class Assignment_1 {
+
+    public static void main(String[] args) {
+        //for loop up starts from zero and ends at 101
+        for (int i = 0; i < 101; i++) {
+            //If the number is divisible by 2 
+            if (i % 2 == 0) {   //print each number on a new line
+                System.out.println(i);
+            }
+
+        }
+
+    }
+
+}
